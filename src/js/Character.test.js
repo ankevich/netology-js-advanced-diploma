493536bb57138de930d4fed783ed90bed4b58984
Annie Bauer <overheads165@gmail.com>
2022-07-25 22:42:26 +0300

    character test

--- src/js/Character.test.js
+++ src/js/Character.test.js
@@ -1,5 +1,15 @@
 import Character from "./Character";
 
-test('Character class should not be instantiable', () => {
+test('нельзя создавать экземпляры Character', () => {
   expect(() => new Character()).toThrowError('Нельзя создавать экземпляры базового класса');
+})
+
+test('можно создавать экземпляры класса унаследованных от Character', () => {
+  class Daemon extends Character {
+    constructor(level) {
+      super(level);
+    }
+  }
+  const newCharacter = new Daemon(1);
+  expect(newCharacter.level).toBe(1);
 })
