7ac19e6e3a1a1d80890fb4615655391afa35d090
Annie Bauer <overheads165@gmail.com>
2022-07-25 16:21:00 +0300

    eslint, jest, babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devServer: {
-        open: true,
-        static: './src',
-        hot: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+  devServer: {
+    open: true,
+    static: './src',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html",
-      })
+      },
     ],
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
