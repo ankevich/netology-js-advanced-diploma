536d2cc73f0c35b877b896f25fc294b2ed96fa42
Annie Bauer <overheads165@gmail.com>
2022-07-25 17:47:58 +0300

    отрисовка доски

--- webpack.config.js
+++ webpack.config.js
@@ -25,19 +25,6 @@ module.exports = {
           },
         },
       },
-      // {
-      //   test: /\.(png|jpe?g|gif)$/i,
-      //   use: [
-      //     {
-      //       loader: "file-loader",
-      //       options: {
-      //         name: "[path][name].[ext]",
-      //         outputPath: "images/",
-      //         publicPath: "images/",
-      //       },
-      //     },
-      //   ],
-      // },
     ],
   },
   plugins: [
