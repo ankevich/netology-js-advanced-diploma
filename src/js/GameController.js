934a43b5df22e959ce0eca4ec75a0a895502815f
Annie Bauer <overheads165@gmail.com>
2022-07-26 19:42:14 +0300

    привязали основные методы

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -7,21 +7,66 @@ export default class GameController {
   }
 
   init() {
+    // Инициализируем UI
     this.gamePlay.drawUi(themes.prairie) 
 
-    // add event listeners to gamePlay events
-    // TODO: load saved stated from stateService
+
+    // -------------------------------------
+    // Добавляем обработчики игровых событий
+    // -------------------------------------
+    
+    // Наведение на клетку
+    this.gamePlay.addCellEnterListener(this.onCellEnter)
+    
+    // Выход из клетки
+    this.gamePlay.addCellLeaveListener(this.onCellLeave)
+    
+    // Клик по клетке
+    this.gamePlay.addCellClickListener(this.onCellClick)
+
+
+    // -------------------------------------
+    // Загрузка и сохранение стейта игры
+    // -------------------------------------
+
+    // Клик по кноке New Game
+    this.gamePlay.addNewGameListener(this.onNewGame)
+
+    // Клик по кнопке Save Game
+    this.gamePlay.addSaveGameListener(this.onSaveGame)
+
+    // Клик по кнопке Load Game
+    this.gamePlay.addLoadGameListener(this.onLoadGame)
+
+    
   }
 
   onCellClick(index) {
     // TODO: react to click
+    console.log(`Cell ${index} clicked`);
   }
 
   onCellEnter(index) {
-    // TODO: react to mouse enter
+    const character = this.stateService.getCharacter(index);
+    if (character) {
+      const message = `🎖${character.level} ⚔${character.attack} 🛡${character.defence} ❤${character.health}`;
+      this.gamePlay.showCellTooltip(message, index);
+    }
   }
 
   onCellLeave(index) {
-    // TODO: react to mouse leave
+    this.gamePlay.hideCellTooltip(index);
+  }
+
+  onNewGame() {
+    console.log('New game');
+  }
+
+  onSaveGame() {
+    console.log('Save game');
+  }
+
+  onLoadGame() {
+    console.log('Load game');
   }
 }
